312f9fb1a61fdbb5e4d3366a0f32a8b72bbff79d
Heroblack-Null <heroblack303@gmail.com>
2024-01-06 13:02:21 +0700

    up 1

--- scripts/blocks/storage.js
+++ scripts/blocks/storage.js
@@ -4,7 +4,7 @@ const miniCore = extend(CoreBlock, "mini-core", {
 	buildCostMultiplier: 3,
 //is it possible to build a core regardless of another core, true or false.
 canPlaceOn(tile, team, rotation){
-        return true;
+        return false;
     },
 //can this core replace another one
     canReplace(other){
@@ -14,6 +14,31 @@ canPlaceOn(tile, team, rotation){
 //is it possible to break the core
     canBreak(tile){
     	return false;
+    },
+//adding build time for the core
+    setStats(){
+this.super$setStats();
+if(this.canBeBuilt() && this.requirements.length > 0){
+this.stats.add(Stat.buildTime, this.buildCost / 60, StatUnit.seconds);
+        }
+    }
+})
+//name of object, type of your content, file-name
+const stoneCore = extend(CoreBlock, "stone-core", {
+	targetable: false,
+	buildCostMultiplier: 3,
+//is it possible to build a core regardless of another core, true or false.
+canPlaceOn(tile, team, rotation){
+        return true;
+    },
+//can this core replace another one
+    canReplace(other){
+        if(other instanceof CoreBlock) return true;
+        return this.super$canReplace(other);
+    },
+//is it possible to break the core
+    canBreak(tile){
+    	return true;
     },
 //adding build time for the core
     setStats(){
